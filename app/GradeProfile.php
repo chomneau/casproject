371740b3fcd60f14aafd39a721164791a6ac7ca3
chomneau <menchomneau@gmail.com>
2019-01-02 08:11:24 +0700

    fix model

--- app/GradeProfile.php
+++ app/GradeProfile.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class GradeProfile extends Model
 {
     public function studentProfile(){
-    	return $this->hasMany(studentProfile::class);
+    	return $this->hasMany(StudentProfile::class);
     }
 
     public function assignment(){
