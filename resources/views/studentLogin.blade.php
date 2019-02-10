700ae053d4be3e4d208d2b8a2e2ffec9cad545eb
chomneau <menchomneau@gmail.com>
2019-02-10 10:09:45 +0700

    update login header

--- resources/views/studentLogin.blade.php
+++ resources/views/studentLogin.blade.php
@@ -6,7 +6,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{asset('StudentLogin/images/icons/favicon.ico')}}"/>
+	
 	
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="{{asset('StudentLogin/vendor/bootstrap/css/bootstrap.min.css') }}">
