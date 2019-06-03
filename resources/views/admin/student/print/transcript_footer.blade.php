0034b3d015c32afc49d88be9e742fe90c23b70d4
chomneau <menchomneau@gmail.com>
2019-06-03 11:42:27 +0700

    update round up

--- resources/views/admin/student/print/transcript_footer.blade.php
+++ resources/views/admin/student/print/transcript_footer.blade.php
@@ -1,88 +1,95 @@
-	<div class="row mt-4" style="margin-top:20px">
-		<div class="col-md-1"></div>
-		<div class="col-md-7 border border-secondary" >
-			<table class="table table-sm" style="font-size: 10px ">
-				<thead>
-			        <tr>			            
-			            Grading Equivalence and Symbols Used
-			        </tr>
-			    </thead>
-				<tbody>
-					<tr style="font-size:14px">
-						<td><b>Excellent</b></td>
-						<td>A</td>
-						<td>4.00</td>
-						<td>93-100%</td>
-						<td>A-</td>
-						<td>3.70</td>
-						<td>90-92%</td>
-						<td></td>
-					</tr>
-					<tr style="font-size:14px">
-						<td><b>Good</b></td>
-						<td>B+</td>
-						<td>3.30</td>
-						<td>87-89%</td>
-
-						<td>B</td>
-						<td>3.00</td>
-						<td>83-86%</td>
-
-						<td>B-</td>
-						<td>2.70</td>
-						<td>80-82%</td>
-					</tr>
-					<tr style="font-size:14px">
-						<td><b>Satisfactory</b></td>
-						<td>C+</td>
-						<td>2.30</td>
-						<td>77-79%</td>
-
-						<td>C</td>
-						<td>2.00</td>
-						<td>73-76%</td>
-
-						<td>C-</td>
-						<td>1.70</td>
-						<td>70-72%</td>
-					</tr>
-					<tr style="font-size:14px">
-						<td><b>Poor</b></td>
-						<td>D+</td>
-						<td>1.30</td>
-						<td>67-69%</td>
-
-						<td>D</td>
-						<td>1.00</td>
-						<td>63-66%</td>
-
-						<td>D-</td>
-						<td>0.70</td>
-						<td>60-62%</td>
-					</tr>
-					<tr style="font-size:14px">
-						<td><b>Failed</b></td>
-						<td>F</td>
-						<td>0.00</td>
-						<td>0.00-59%</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
+<div class="container">
 		
-		<div class="col-md-4"></div>
-
-
-		<div class="col-md-6 offset-3 mt-4">
+		<div class="row mt-4">
+			<div class="col-md-2"></div>
 			
-			<p class="text-uppercase text-center" style="font-size: 12px">
-				****** any entry below this lines in not valid ******* 
-			</p>
-
-		</div>
-
+			<div class="col-md-8 border border-secondary" >
+				<table class="table table-sm" style="font-size: 10px ">
+					<thead>
+								<tr>			            
+										Grading Equivalence and Symbols Used
+								</tr>
+						</thead>
+					<tbody>
+						<tr style="font-size:12px">
+							<td><b>Excellent</b></td>
+							<td>A</td>
+							<td>4.00</td>
+							<td>93-100%</td>
+							<td>A-</td>
+							<td>3.70</td>
+							<td>90-92%</td>
+							<td></td>
+						</tr>
+						<tr style="font-size:12px">
+							<td><b>Good</b></td>
+							<td>B+</td>
+							<td>3.30</td>
+							<td>87-89%</td>
+	
+							<td>B</td>
+							<td>3.00</td>
+							<td>83-86%</td>
+	
+							<td>B-</td>
+							<td>2.70</td>
+							<td>80-82%</td>
+						</tr>
+						<tr style="font-size:12px">
+							<td><b>Satisfactory</b></td>
+							<td>C+</td>
+							<td>2.30</td>
+							<td>77-79%</td>
+	
+							<td>C</td>
+							<td>2.00</td>
+							<td>73-76%</td>
+	
+							<td>C-</td>
+							<td>1.70</td>
+							<td>70-72%</td>
+						</tr>
+						<tr style="font-size:12px">
+							<td><b>Poor</b></td>
+							<td>D+</td>
+							<td>1.30</td>
+							<td>67-69%</td>
+	
+							<td>D</td>
+							<td>1.00</td>
+							<td>63-66%</td>
+	
+							<td>D-</td>
+							<td>0.70</td>
+							<td>60-62%</td>
+						</tr>
+						<tr style="font-size:12px">
+							<td><b>Failed</b></td>
+							<td>F</td>
+							<td>0.00</td>
+							<td>0.00-59%</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col-md-2"></div>
+		</div>	
+	{{-- end row table --}}
+		
+	<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8 mt-4">
+			
+					<p class="text-uppercase text-center" style="font-size: 12px">
+						****** any entry below this lines in not valid ******* 
+					</p>
+		
+				</div>
+			<div class="col-md-2"></div>
 
 	</div>
+
+		
 
 
 			<div class="col-md-10 offset-1" style="margin-top: 2em">
@@ -111,5 +118,6 @@
 			</p>
 		</div>
 
+	</div>
 
 
